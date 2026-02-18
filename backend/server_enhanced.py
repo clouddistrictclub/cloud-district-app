e4ede5808da320361207d8a61ba4976dc23b99f2
clouddistrictclub <jkaatz@gmail.com>
2026-02-17 22:45:10 -0600

    Update server_enhanced.py

--- backend/server_enhanced.py
+++ backend/server_enhanced.py
@@ -722,5 +722,5 @@ import os
 import uvicorn
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run("server_enhanced:app", host="0.0.0.0", port=port)
